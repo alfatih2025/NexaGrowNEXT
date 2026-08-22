34b2dd437cf61aeeb4de236c99c057dc77572487
Habibullah Naja Alfatih Wibowo <alfatihwibowo264@gmail.com>
2026-08-22 12:13:37 +0700

    api

--- src/api-routes/airouter.js
+++ src/api-routes/airouter.js
@@ -4,7 +4,7 @@ import { getAiRouterStatus, sendAiRouterMessage, isArduinoFormulaRequest, buildF
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key');
 
   if (req.method === 'OPTIONS') return res.status(204).end();
   if (!requireApiAuth(req, res)) return;
